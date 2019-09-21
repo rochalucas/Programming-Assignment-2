82d66e59d9430ce448e37a6c7b6ead1fcda10cc2
rochalucas <lucasrochasilva@hotmail.com>
2019-09-21 13:37:46 -0400

    My first commit

---
+++ Rocha_p2/src/Rocha_p2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Rocha_p2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int userOption;
+		double weight;
+		double height;
+		double bmi;
+
+		System.out.println("Press 1 if you wish to use pounds (lbs) and inches (in).");
+		System.out.println("Press 2 if you wish to use Kilograms (kgs) and meters (m)");
+		userOption = in.nextInt();
+		
+		if (userOption == 1) {
+			System.out.println("Please enter your weight in pounds.");
+			weight = in.nextDouble();
+			
+			System.out.println("Please enter you height in inches.");
+			height = in.nextDouble();
+			
+			bmi = (703 * weight)/(height * height);
+			
+			System.out.printf("\nYour BMI is: %.2f\n\n",  bmi);
+			System.out.println("\nAccording to the National Heart Lung and Blood Institute, these are the BMI categories:\n");
+			System.out.println("<18.5: Underweight.\n18.5 - 24.9: Normal Weight.\n25 - 29.9: Overweight.\n30 or greater: Obese.\n");
+			
+			
+		}
+		
+		else if (userOption == 2) {
+			System.out.println("Please enter your weight in kilograms.");
+			weight = in.nextDouble();
+			
+			System.out.println("Please enter you height in meters.");
+			height = in.nextDouble();
+			
+			bmi = weight/(height * height);
+			
+			System.out.printf("\nYour BMI is: %.2f\n\n",  bmi);
+			System.out.println("\nAccording to the National Heart Lung and Blood Institute, these are the BMI categories:\n");
+			System.out.println("<18.5: Underweight.\n18.5 - 24.9: Normal Weight.\n25 - 29.9: Overweight.\n30 or greater: Obese.\n");
+			
+			
+		}
+		
+	}
+
+}
---
+++ Rocha_p2/src/Rocha_p3.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Rocha_p3 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String[] topics = new String [5];
+		int[][] responses = new int[5][10];
+		int[] totalTopic = new int[5];
+		double[] averageTopic = new double[5];
+		int numUsers;
+		int i;
+		int j;
+		int userResponse;
+		int highest = 0;
+		int lowest = 0;
+		double temp1;
+		double temp2;
+		
+		
+		for (i=0; i<5; i++) {
+			totalTopic[i]   = 0;
+			averageTopic[i] = 0;
+			for (j=0; j<10; j++) {
+				responses[i][j] = 0;
+			}
+		}
+		
+		topics[0] = "Income Inequality";
+		topics[1] = "Climate Change";
+		topics[2] = "International Affairs";
+		topics[3] = "Healthcare";
+		topics[4] = "Education";
+		
+		System.out.println("In this survey you will rate the importance of five topics to be discussed in the 2020 presidential debate.");
+		System.out.println("Rates go from 1 (least important) to 10 (most important)");
+		System.out.println("How many people will be taking the survey?");
+		numUsers = in.nextInt();
+		
+		
+		for (i=0; i<5; i++) {
+			System.out.println("Topic: " + topics[i]);
+			for (j=0; j<numUsers; j++) {
+				System.out.println("Rating from user " + (j + 1) + ":");
+				userResponse =  in.nextInt();
+				responses[i][userResponse-1] += 1;
+				totalTopic[i] += userResponse;
+			}
+			averageTopic[i] = (double) totalTopic[i] / numUsers;
+			
+		}
+		
+		System.out.println("Votes for each topic:");
+		System.out.println(		"\t\t\t1   2   3   4   5   6   7   8   9   10    Average Rating");
+		System.out.println(		"\t\t\t--  --  --  --  --  --  --   --  --  --   --------------");
+		
+		for (i=0; i<5; i++) {
+			System.out.print(topics[i] + ":\t");
+			if (i == 1 || i == 3 || i == 4)
+				System.out.print("\t");
+			for (j=0; j<10; j++) {
+				System.out.print("" + responses[i][j] + "   ");
+			}
+				System.out.printf("\t%.2f", averageTopic[i]);
+				System.out.print("\n");
+			}
+		
+		temp1 = averageTopic[0];
+		temp2 = averageTopic[0];
+		
+		for (i=0; i<5; i++) {
+			if (averageTopic[i] > temp1) {
+				temp1 = averageTopic[i];
+				highest = i;
+			}
+			if (averageTopic[i] < temp2) {
+				temp2 = averageTopic[i];
+				lowest = i;
+			}
+		}
+		
+		System.out.print("\nTopic with the highest score: " + topics[highest] + ". Points: ");
+		System.out.printf("%.2f", averageTopic[highest]);
+		System.out.print("\nTopic with the lowest score: " + topics[lowest] + ". Points: ");
+		System.out.printf("%.2f", averageTopic[lowest]);
+	}		
+
+}
+
+
